c94a0bd5bed02e4be393b7ec9d3d782506f8ae45
dchow <dchow@box.com>
2024-08-22 10:56:21 -0400

    starting to implement GA

--- classes/enrichment_classes.py
+++ classes/enrichment_classes.py
@@ -1,4 +1,24 @@
+import random
 from .utilities import clean_enrichment_name
+
+class population:
+
+    def __init__(self, student_list, enrichments):
+        self.students = student_list
+        self.enrichments = enrichment
+
+        for x in self.students:
+            x.randomize_assignment()
+
+        self.genome = [x.assignment for x in self.students]
+
+    def compute_penalty(self):
+        """Computes current cost structure of genome
+        -- cost of a ranked choice is rank - 1
+        -- there is a penalty of +class max per each student over the class maximum
+        -- there is a penalty of +5 per each student missing from the class minimum
+        -- each student waitlisted gets a penalty of last ranked + 1
+        """
 
 class student:
 
@@ -8,12 +28,16 @@ class student:
         self.age = age
         self.name = name
         self.teacher = teacher
+        self.assignment = 0
         self.enrichment_preference = {
-            1: None
+            0: "waitlist"
         }
 
     def assign_preference(self, rank, enrichment_program):
         self.enrichment_preference[rank] = enrichment_program
+
+    def randomize_assignment(self):
+        self.assignment = random.randint(0, len(self.enrichment_preference.keys()) - 1)
 
 class enrichment:
     def __init__(self, enrichment_name, min_size=8, max_size=12, waitlist_size=4):
